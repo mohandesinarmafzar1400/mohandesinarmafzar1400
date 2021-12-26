b6f2ad0a06138a25d3f6cfc0837fbc917641cd70
ali <amr@gmail.com>
2021-12-27 01:17:31 +0330

    End Of MYSQL.py

--- MYSQL/MYSQL.py
+++ MYSQL/MYSQL.py
@@ -3,95 +3,26 @@ import mysql.connector
 ur="root"
 pw="Razi0023161469"
 #Create Database dbfootball and Tables With User & Password Clint
-def CreateDBT(ur, pw):              #(ur,pw,num)
+def Create_DBT(ur, pw):              #(ur,pw,num)
     mydb = mysql.connector.connect(
     host="localhost",
     user=ur,
     password=pw
     )
     mycursor = mydb.cursor()
+    #Create DataBase
     mycursor.execute("CREATE DATABASE IF NOT EXISTS dbfootball")
-    #Create Table countries
-    
+    #Change DataBase
     mycursor.execute("USE dbfootball")
-    
-    #if num == 1 or num == 3:
+    #Create Table Countries
     mycursor.execute("CREATE TABLE IF NOT EXISTS countries (id INT NOT NULL AUTO_INCREMENT UNIQUE, country_id VARCHAR(10) PRIMARY KEY, \
     country_name VARCHAR(100), country_logo VARCHAR(255))")
-    #if num == 2 or num == 3:
+    #Create Table Perosns
     mycursor.execute("CREATE TABLE IF NOT EXISTS persons (id INT NOT NULL AUTO_INCREMENT UNIQUE, uname VARCHAR(255) PRIMARY KEY, \
     ps VARCHAR(100), fname VARCHAR(50), lname VARCHAR(50), gender VARCHAR(6))")
-    # mycursor.execute("SHOW DATABASES")
-    # #Variable Defining
-    # #Variable For Check to Database Exists
-    # Exists_DB = False
-    # Result_DB = False
-    # #Exists_Tables = [countrys,competitions,teams,Player]
-    # #Exists_Tables = [False,False,False]
-    # #Exists_Table = [country]
-    # Exists_Table = False
-    # result_Final = False
-    # for x in mycursor:
-        
-    #     if (x[0] == "dbfootball"):
-    #         Exists_DB = True
-    #         break
-    # if not Exists_DB:
-    #     mycursor.execute("CREATE DATABASE dbfootball")
-    #     Result_DB = True
-    # print(Result_DB)
-    # if Result_DB:
-    #     mycursor.execute("USE dbfootball")
-    #     #Create Table
-    #     for x in mycursor:
-    #         if (x[0] == "country"):
-    #             Exists_Table = True
-
-    #         """elif (x[0] == "competitions"):
-    #             Exists_Tables[1] = True
-    #         elif (x[0] == "teams"):
-    #             Exists_Tables[2] = True"""
-
-    #     #result=[]
-        
-    #     #Create Table countries
-    #     if not Exists_Table:
-    #         mycursor.execute("CREATE TABLE countries (id INT NOT NULL AUTO_INCREMENT UNIQUE, country_id VARCHAR(10) PRIMARY KEY, \
-    #             country_name VARCHAR(100), country_logo VARCHAR(255))")
-    #         result_Final = True
-    #     #Create Table competitions
-    #     # if not Exists_Tables[1]:
-    #     #     mycursor.execute("CREATE TABLE competitions (id INT NOT NULL AUTO_INCREMENT UNIQUE, country_id VARCHAR(10) , \
-    #     #         country_name VARCHAR(100), league_id VARCHAR(10), league_name VARCHAR(100),league_season VARCHAR(11), \
-    #     #         league_logo VARCHAR(255),country_logo VARCHAR(255),FOREIGN KEY (country_id) REFERENCES countries(country_id))")
-    #     #     result.append("True")
-    #     # else :
-    #     #     result.append("True")
-    #     # #Create Table teams
-    #     # if not Exists_Tables[2]:
-    #     #     mycursor.execute("CREATE TABLE teams (id INT NOT NULL AUTO_INCREMENT UNIQUE key , team_key VARCHAR(10) PRIMARY KEY, \
-    #     #         team_name VARCHAR(100), team_badge VARCHAR(255), coach_name VARCHAR(100))")
-    #     #     result.append("True")
-    #     # else :
-    #     #     result.append("True")
-    #     # #Create Table Player    
-    #     # if not Exists[2]:
-    #     #     mycursor.execute("CREATE TABLE players (id INT NOT NULL AUTO_INCREMENT UNIQUE key , player_id VARCHAR(15) PRIMARY KEY, \
-    #     #         player_image VARCHAR(255), player_name VARCHAR(100), player_number VARCHAR(2),player_type VARCHAR(50),\
-    #     #         player_match_played VARCHAR(5),player_goals VARCHAR(5),player_yellow_cards VARCHAR(2),\
-    #     #         player_red_cards VARCHAR(2),player_injured VARCHAR(5),player_substitute_out VARCHAR(3),\
-    #     #         player_substitutes_on_bench VARCHAR(2),player_assists VARCHAR(2),team_name VARCHAR(50),\
-    #     #         team_key VARCHAR(15),FOREIGN KEY (team_key) REFERENCES teams(team_key))")
-    #     #     result.append("True")
-    #     # else :
-    #     #     result.append("True")
-    #     # if result == ["True", "True", "True"]:
-    #     #     return True
-    # return result_Final
-
-
+    
 #Drop Database dbfootball
-def DropDB(ur,pw):
+def Drop_DB(ur,pw):
     mydb = mysql.connector.connect(
     host="localhost",
     user=ur,
@@ -100,23 +31,6 @@ def DropDB(ur,pw):
     mycursor = mydb.cursor()
     mycursor.execute("DROP DATABASE IF EXISTS dbfootball")
     
-    # Exists = False
-    # #For Exists Database Or Not
-    # result = False
-    # #For The Final Result
-    # mycursor.execute("SHOW DATABASES")
-    # for x in mycursor:
-    #     if (x[0] == "dbfootball"):
-    #         Exists = True
-    #         break
-    # #Check DataBase Exists
-    # if Exists:
-    #     mycursor.execute("drop databases dbfootball")
-    #     result = True
-    # else :
-    #     result = False
-    # #The result is whether can drop or not
-    # return result
 
 def Insert_iteams(ur,pw,table_name,*args):
     mydb = mysql.connector.connect(
@@ -129,15 +43,17 @@ def Insert_iteams(ur,pw,table_name,*args):
     #Check Exists Database Or Not
     Exists_DB = [x for x in mycursor]
     if len(Exists_DB) == 0:
-        CreateDBT(ur, pw)
+        Create_DBT(ur, pw)
     mycursor.execute("USE dbfootball")
     #if not 
+
     #Defining Variable For Work
     args = list(args)
     statement_cols = "INSERT INTO {} (".format(table_name)    
     statement_vals = "VALUES ("
     vals=[]
     statement_final = ""
+    #For Country Insert_Iteams
     if (table_name == "countries"):
         for i in range(3):
             if (i == 0):
@@ -151,9 +67,14 @@ def Insert_iteams(ur,pw,table_name,*args):
         statement_cols += ") "
         statement_vals += ")"
         for i in range( 0 , len(args) , 3 ):
-                vals.append( (args[ i ], args [ i + 1 ], args[ i + 2 ]) )
+                if (Check_PCE(ur,pw,"countries",args[0])):
+                    #If Exists Country In Table Countriies With Primary Key That As Input 
+                    vals.append( (args[ i ], args [ i + 1 ], args[ i + 2 ]) )
+                else :
+                    #Do Noting
+                    pass
         statement_final = statement_cols + statement_vals
-    #For persons
+    #For Cersons Insert_Iteams
     elif (table_name == "persons"):
         for i in range(5):
             if (i == 0):
@@ -168,11 +89,74 @@ def Insert_iteams(ur,pw,table_name,*args):
         statement_cols += ") "
         statement_vals += ")"
         for i in range( 0 , len(args) , 5 ):
-                vals.append( (args[ i ], args [ i + 1 ], args[ i + 2 ], args[ i + 3 ], args[ i + 4 ]) )
+                if (Check_PCE(ur,pw,"persons",args[0])):
+                    #If Exists Person In Table Persons With Primary Key That As Input 
+                    vals.append( (args[ i ], args [ i + 1 ], args[ i + 2 ], args[ i + 3 ], args[ i + 4 ]) )
+                else :
+                    #Do Noting
+                    pass
         statement_final = statement_cols + statement_vals
     #Elif For Othe Tables
     mycursor.executemany(statement_final,vals)
     mydb.commit()
-#CreateDBT(ur,pw)
-#Insert_iteams(ur, pw, "persons","uname","ps","fname","lname","gender","232421213","Razi321","amirmahdi","Razi","male")
+
+#Check Person Or Country Exists
+def Check_PCE(ur,pw,table_name,val):
+    mydb = mysql.connector.connect(
+    host="localhost",
+    user=ur,
+    password=pw,
+    )
+    mycursor = mydb.cursor()
+    mycursor.execute("SHOW DATABASES LIKE 'dbfootball'")
+    #Check Exists Database Or Not
+    Exists_DB = [x for x in mycursor]
+    if len(Exists_DB) == 0:
+        Create_DBT(ur, pw)
+    mycursor.execute("USE dbfootball")
+    #if not 
+    val='\'{}\''.format(val)
+    #Defining Variable For Work
+    if (table_name == "persons"):
+        statement_final = "SELECT uname FROM persons WHERE uname = {}".format(val)
+    elif (table_name == "countries"):
+        statement_final = "SELECT country_id FROM countries WHERE country_id = '{}'".format(val)
+    #Elif For Othe Tables
+    mycursor.execute(statement_final)
+    for x in mycursor:
+        if len(x) == 1:
+            return False
+    else :
+        return True
+
+
+#Select Iteam Form Table Countries
+def Select_IC(ur,pw):
+    mydb = mysql.connector.connect(
+    host="localhost",
+    user=ur,
+    password=pw,
+    )
+    mycursor = mydb.cursor()
+    mycursor.execute("SHOW DATABASES LIKE 'dbfootball'")
+    #Check Exists Database Or Not
+    Exists_DB = [x for x in mycursor]
+    if len(Exists_DB) == 0:
+        Create_DBT(ur, pw)
+    mycursor.execute("USE dbfootball")
+    #if not 
+    
+    statement_final = "SELECT * FROM countries"
+
+    #Elif For Othe Tables
+    mycursor.execute(statement_final)
+    #Defining Variable For Work
+    temp = mycursor.fetchall()
+    return temp
+
+#Drop_DB(ur,pw)    
+#Create_DBT(ur,pw)
+#Select_Iteams(ur,pw,"persons","uname","123")
+#Insert_iteams(ur, pw, "persons","uname","ps","fname","lname","gender","2324 1221213","Raz i321","amirm ahdi","Ra zi","m ale")
 #Insert_iteams(ur, pw, "countries","country_id","country_name","country_logo","amir1123","Razi321","amirmahdi")
+#Insert_iteams(ur, pw,"persons","uname","ps","fname","lname","gender","amir1138","1","2","3","4")
